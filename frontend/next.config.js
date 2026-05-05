21c51607ce62105153cc824aba93a646d26cb32a
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2026-05-05 15:13:08 +0600

    Feat: Build & Deploy with fix done.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // This tells Next.js to generate a static 'out' folder
   images: {
+    unoptimized: true, // Required for static exports unless using a 3rd party loader
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'randomuser.me' },
     ],
   },
-}
+};
 
 module.exports = nextConfig
